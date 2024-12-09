ca1d05dbdced955977529dca772b2a7df94cc2b1
AnkulKumar99 <manish@gomilestone.com>
2024-12-09 11:46:40 +0530

    add counter section

--- script.js
+++ script.js
@@ -143,3 +143,36 @@ form.addEventListener('submit', function (e) {
             }, 4000);
         });
 });
+
+// Counter*********************************
+
+var counted = 0;
+$(window).scroll(function() {
+
+  var oTop = $('#counter').offset().top - window.innerHeight;
+  if (counted == 0 && $(window).scrollTop() > oTop) {
+    $('.count').each(function() {
+      var $this = $(this),
+        countTo = $this.attr('data-count');
+      $({
+        countNum: $this.text()
+      }).animate({
+          countNum: countTo
+        },
+
+        {
+          duration: 2000,
+          easing: 'swing',
+          step: function() {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function() {
+            $this.text(this.countNum);
+          }
+
+        });
+    });
+    counted = 1;
+  }
+
+});
